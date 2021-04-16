66f530af3c56a35617840e630f4030723d3fcf45
minchao920917 <499790879@qq.com>
2021-04-16 23:43:55 +0800

    ceshi

---
+++ bin/mcf.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const program = require("commander");
+const chalk = require("chalk");
+const { promisify } = require("util");
+const figlet = require("figlet");
+const clear = require("clear");
+const log = (content) => console.log(chalk.green(content));
+
+program.version(require("../package").version);
+
+// program
+//     .command('init <name>')
+//     .description('init project')
+//     .action(
+//         require('../lib/init')
+//     )
+
+// 打印欢迎画⾯
+// clear();
+log(figlet.textSync("mcf welcome"));
+
+program
+  .command("基本用法", "----------------")
+  .usage("<command> [options]")
+  .command("add", "添加新的源 mcf add")
+  .command("delete", "删除源 mcf delete")
+  .command("list", "展示源列表 mcf list")
+  .command("init", "从源初始化项目 mcf init <模板名> 项目名")
+  .command("工程化项目", "---------------")
+  .usage("<command> [options]")
+  .command("vue-admin", "快速构建新vue项目 mcf vue-admin <项目名>");
+  program
+  .command('vue-app <projectName>')
+    .description('快速构建新vue-app项目')
+    .action(
+        require('../lib/mcf-vue-app')
+    )
+program.parse(process.argv);
